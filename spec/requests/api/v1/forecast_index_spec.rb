33b53351fedc35b3bb41d2b3207ff0ea2865aa9a
Gvieve <gnuebel13@gmail.com>
2021-04-24 14:55:52 -0600

    Test all data points for current, daily and hourly weather in forecast request

--- spec/requests/api/v1/forecast_index_spec.rb
+++ spec/requests/api/v1/forecast_index_spec.rb
@@ -9,7 +9,7 @@ describe 'Forecast API' do
           get "/api/v1/forecast?location=#{location}"
 
           json = JSON.parse(response.body, symbolize_names: true)
-          # require "pry"; binding.pry
+
           expect(response).to be_successful
           expect(response.status).to eq(200)
           expect(json).to be_a(Hash)
@@ -29,8 +29,94 @@ describe 'Forecast API' do
         end
       end
 
-      it "the weather data has the current_weather data" do
+      describe 'the weather data for valid request includes' do
+        it "current_weather data" do
+          VCR.use_cassette('requests/api/v1/forecast') do
+            location = 'denver,co'
+            get "/api/v1/forecast?location=#{location}"
 
+            json = JSON.parse(response.body, symbolize_names: true)
+            current = json[:data][:attributes][:current_weather]
+
+            expect(response).to be_successful
+            expect(response.status).to eq(200)
+            expect(current).to be_a(Hash)
+            expect(current).to have_key(:datetime)
+            expect(current[:datetime]).to be_a(String)
+            expect(current).to have_key(:sunrise)
+            expect(current[:sunrise]).to be_a(String)
+            expect(current).to have_key(:sunset)
+            expect(current[:sunset]).to be_a(String)
+            expect(current).to have_key(:temperature)
+            expect(current[:temperature]).to be_a(Float)
+            expect(current).to have_key(:feels_like)
+            expect(current[:feels_like]).to be_a(Float)
+            expect(current).to have_key(:humidity)
+            expect(current[:humidity]).to be_a(Float).or be_an(Integer)
+            expect(current).to have_key(:uvi)
+            expect(current[:uvi]).to be_a(Float).or be_an(Integer)
+            expect(current).to have_key(:visibility)
+            expect(current[:visibility]).to be_a(Float).or be_an(Integer)
+            expect(current).to have_key(:conditions)
+            expect(current[:conditions]).to be_a(String)
+            expect(current).to have_key(:icon)
+            expect(current[:icon]).to be_a(String)
+          end
+        end
+
+        it "5 days of daily_weather data" do
+          VCR.use_cassette('requests/api/v1/forecast') do
+            location = 'denver,co'
+            get "/api/v1/forecast?location=#{location}"
+
+            json = JSON.parse(response.body, symbolize_names: true)
+            daily = json[:data][:attributes][:daily_weather]
+
+            expect(response).to be_successful
+            expect(response.status).to eq(200)
+            expect(daily).to be_an(Array)
+            expect(daily.count).to eq(5)
+            expect(daily.first).to be_a(Hash)
+            expect(daily.first).to have_key(:date)
+            expect(daily.first[:date]).to be_a(String)
+            expect(daily.first).to have_key(:sunrise)
+            expect(daily.first[:sunrise]).to be_a(String)
+            expect(daily.first).to have_key(:sunset)
+            expect(daily.first[:sunset]).to be_a(String)
+            expect(daily.first).to have_key(:max_temp)
+            expect(daily.first[:max_temp]).to be_a(Float)
+            expect(daily.first).to have_key(:min_temp)
+            expect(daily.first[:min_temp]).to be_a(Float)
+            expect(daily.first).to have_key(:conditions)
+            expect(daily.first[:conditions]).to be_a(String)
+            expect(daily.first).to have_key(:icon)
+            expect(daily.first[:icon]).to be_a(String)
+          end
+        end
+
+        it "8 hours of hourly_weather data" do
+          VCR.use_cassette('requests/api/v1/forecast') do
+            location = 'denver,co'
+            get "/api/v1/forecast?location=#{location}"
+
+            json = JSON.parse(response.body, symbolize_names: true)
+            hourly = json[:data][:attributes][:hourly_weather]
+
+            expect(response).to be_successful
+            expect(response.status).to eq(200)
+            expect(hourly).to be_an(Array)
+            expect(hourly.count).to eq(8)
+            expect(hourly.first).to be_a(Hash)
+            expect(hourly.first).to have_key(:time)
+            expect(hourly.first[:time]).to be_a(String)
+            expect(hourly.first).to have_key(:temperature)
+            expect(hourly.first[:temperature]).to be_a(Float)
+            expect(hourly.first).to have_key(:conditions)
+            expect(hourly.first[:conditions]).to be_a(String)
+            expect(hourly.first).to have_key(:icon)
+            expect(hourly.first[:icon]).to be_a(String)
+          end
+        end
       end
     end
   end
