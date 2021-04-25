6ed9974ec55997f280f555a51ab1e0f0bd7c3594
Gvieve <gnuebel13@gmail.com>
2021-04-25 16:53:46 -0600

    Finish happy path test for User registration

--- spec/requests/api/v1/user_registration_spec.rb
+++ spec/requests/api/v1/user_registration_spec.rb
@@ -9,17 +9,32 @@ describe 'User Registration API' do
           password: 'littleone',
           password_confirmation: 'littleone'
         }
-
         headers = {"CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json"}
 
         post "/api/v1/users", headers: headers, params: user_registration_body.to_json
-        require "pry"; binding.pry
 
-        user = User.find_by(email: user_registration_body[:email])
-        expect(response.content_type).to eq("application/json; charset=utf-8")
+        user = User.last
+        expect(user.email).to eq(user_registration_body[:email])
+
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.content_type).to eq("application/json")
         expect(response).to be_successful
         expect(response.status).to eq(201)
-        expect(user.email).to eq(user_registration_body[:email])
+        expect(json).to be_a(Hash)
+        expect(json).to have_key(:data)
+        expect(json[:data]).to be_a(Hash)
+        expect(json[:data]).to have_key(:type)
+        expect(json[:data][:type]).to eq('users')
+        expect(json[:data]).to have_key(:id)
+        expect(json[:data][:id]).to be_a(String)
+        expect(json[:data]).to have_key(:attributes)
+        expect(json[:data][:attributes]).to be_a(Hash)
+        expect(json[:data][:attributes]).to have_key(:email)
+        expect(json[:data][:attributes][:email]).to be_a(String)
+        expect(json[:data][:attributes]).to have_key(:api_key)
+        expect(json[:data][:attributes][:api_key]).to be_a(String)
+        expect(json[:data][:attributes][:api_key].length).to eq(28)
       end
     end
   end
