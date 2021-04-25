718867ba4ddded6b73146570f0f5e00dba993afd
Gvieve <gnuebel13@gmail.com>
2021-04-25 16:59:08 -0600

    Create the Users serializer and controller action with param validation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,26 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    require "pry"; binding.pry
+    return invalid_request if !required.all? {|key| params.has_key? key }
+    new_user = User.new(user_params)
+    if new_user.save
+      render json: UsersSerializer.new(new_user), status: :created
+    end
+  end
+
+  private
+
+  def invalid_request
+    error = "Please send a valid request, missing required information"
+    render_error(error)
+  end
+
+  def required
+    [:email, :password, :password_confirmation]
+  end
+
+  def user_params
+    params[:email] = params[:email].downcase
+    params.permit(:email, :password, :password_confirmation)
   end
 end
