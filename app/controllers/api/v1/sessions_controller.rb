d7d0e1d3d032dca9506a77725eba5ba836d3e877
Gvieve <gnuebel13@gmail.com>
2021-04-27 20:59:45 -0600

    Create sessions facade spec and file, refactor controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,18 +1,12 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    return render_invalid_params if !required.all? {|key| params.has_key? key }
-    user = User.find_by(email: params[:email].downcase)
-    if user && user.authenticate(params[:password])
+    user = SessionsFacade.authenticate_user(params)
+    if user == 'invalid'
+      render_invalid_params
+    elsif user
       render json: UsersSerializer.new(user), status: :created
     else
-      error = "The email or password you entered is incorrect"
-      render_error(error)
+      render_error("The email or password you entered is incorrect")
     end
-  end
-
-  private
-
-  def required
-    [:email, :password]
   end
 end
